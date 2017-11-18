600f435003d1bcaee29d945cf28909a48034a824
joshuaaron <josh_reynolds_@hotmail.com>
2017-11-18 21:14:32 +1100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -71,17 +71,3 @@ module.exports = {
 	]), 
 	]
 };
-
-// sass-loader - loader to connect to webpack
-// node-sass will do the translation
-// css-loader to do something with the css as we can't handle it alone in JS
-// extract-text-webpack-plugin which will allow us to get our compiled css code and place it into it's own file to import - which decouples it.
-// babel-core - will do the translating (like node-sass)
-// babel-loader - loader to connect to webpack
-// babel-preset-env 
-// html-loader 
-// html-webpack-plugin - create/recreate file based on one we use as a source/template
-// template option: with no config, creates new file with dummy title and auto inject bundles.
-// with template config. still creates index.html, injects bundles BUT will take our html template and use that as our source, which is parsed by HTML-loader
-// and sets the HTML for the page.
-// --save-dev
